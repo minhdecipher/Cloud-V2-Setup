036d68f2a751b9adfb470b94cacf36ace27c9a92
Minh Bui <minhbui85@gmail.com>
2016-04-04 14:13:30 -0700

    updated script hostname

--- automation/brightcove/monthly_bc.sh
+++ automation/brightcove/monthly_bc.sh
@@ -4,22 +4,21 @@ DIR="$(dirname $0)"
 cd "$DIR"
 
 #Server
-SERVER="https://$(hostname)"
-SERVER_SHORT="$(hostname -s)"
+SERVER="$(hostname -s)"
 
 #Get today's date in format YYYY/MM/DD so the filenames will be unique.
 DATESTART=`date -d'last month' +%m/01/%Y`
 DATEEND=`date -d'yesterday' +%m/%d/%Y`
 MONTHYEAR=`date -d'last month' +%B%Y`
 
-FILENAME="${SERVER_SHORT}_${MONTHYEAR}_brightcove_usage"
+FILENAME="${SERVER}_${MONTHYEAR}_brightcove_usage"
 
 #Use the Decipher shell generate command to generate data.
 hpython brightcove-collect-views.py ${DATESTART} ${DATEEND} > ${FILENAME}.txt
 td2xls ${FILENAME}.xls ${FILENAME}.txt
 
 #Let's email the file to some users using the server's mailimp script.
-SUBJECT="Brightcove Usage From ${DATESTART} - ${DATEEND}. Server: ${SERVER}"
+SUBJECT="Brightcove Usage From ${DATESTART} - ${DATEEND}. Server: ${SERVER}.decipherinc.com"
 BODY="Brightcove Usage From ${DATESTART} - ${DATEEND} Attached"
 
 #Created a second variable for easier programmer testing.
