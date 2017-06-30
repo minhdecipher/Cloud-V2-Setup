8f0f8d7fa207ec2e53d77d9bd3f12d677cf4eb4e
Minh Bui <minh@decipherinc.com>
2017-06-30 11:46:50 +0300

    Update script to use latest bc-views script

--- automation/brightcove/monthly_bc.sh
+++ automation/brightcove/monthly_bc.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
+#Server & Home
 DIR="$(dirname $0)"
-cd "$DIR"
-
-#Server
+V2HOME="/home/hermes/v2"
 SERVER="$(hostname -s)"
 
 #Get today's date in format YYYY/MM/DD so the filenames will be unique.
@@ -13,8 +12,10 @@ MONTHYEAR=`date -d'last month' +%B%Y`
 
 FILENAME="${SERVER}_${MONTHYEAR}_brightcove_usage"
 
-#Use the Decipher shell generate command to generate data.
-hpython brightcove-collect-views.py ${DATESTART} ${DATEEND} > ${FILENAME}.txt
+cd "$V2HOME"
+here brightcove-views.py ${DATESTART} ${DATEEND} > ${DIR}/${FILENAME}.txt
+cd "$DIR"
+
 td2xls ${FILENAME}.xls ${FILENAME}.txt
 
 #Let's email the file to some users using the server's mailimp script.
@@ -22,11 +23,11 @@ SUBJECT="Brightcove Usage From ${DATESTART} - ${DATEEND}. Server: ${SERVER}.deci
 BODY="Brightcove Usage From ${DATESTART} - ${DATEEND} Attached"
 
 #Created a second variable for easier programmer testing.
-RECIPIENTS="csm@decipherinc.com"
-#RECIPIENTS="minh@decipherinc.com"
+RECIPIENTS="csm@focusvision.com"
+#RECIPIENTS="minh@focusvision.com"
 
 #Execute the email command
-echo -e "${BODY}" | mailimp --fromAddress minh@decipherinc.com -s "${SUBJECT}" -F "${FILENAME}.xls" ${RECIPIENTS}
+echo -e "${BODY}" | mailimp --fromAddress minh@focusvision.com -s "${SUBJECT}" -F "${FILENAME}.xls" ${RECIPIENTS}
 
 rm ${FILENAME}.txt
 rm ${FILENAME}.xls
